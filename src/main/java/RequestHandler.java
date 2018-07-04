b7a5051aa532c5c9fab3d8597b5398016b36a67a
MANNBOU\eisuke <l85a2556@gmail.com>
2018-07-04 23:38:58 +0900

    RequestHandler 完成

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -10,7 +10,7 @@ import main.java.enums.Status;
 public class RequestHandler {
 	private static Path BAD_REQUEST_HTML_PATH	 = Paths.get("public/400.html");
 	private static Path FORBIDDEN_HTML_PATH  	 = Paths.get("public/403.html");
-	private static Path NOT_FOUND_HTMLPATH	 = Paths.get("public/404.html");
+	private static Path NOT_FOUND_HTML_PATH	 = Paths.get("public/404.html");
 
 	private static String HTML_MIME = "text/html;charset=utf8";
 	private static MimeDetector mimeDetector = new MimeDetector("mimes.properties");
@@ -26,11 +26,21 @@ public class RequestHandler {
 		if (!resourcePath.startsWith("public/")){// ディレクトリトラバーサル
 			return new Response(Status.FORBIDDEN, HTML_MIME, Files.readAllBytes(FORBIDDEN_HTML_PATH));
 		}
-		if (Files.isReadable(resourcePath)) {
-			String mime = mimeDetector.getMime(resourcePath);
 
+
+
+		if(Files.isDirectory(resourcePath)) {
+			String mime = mimeDetector.getMime(resourcePath);
+			return new Response(Status.OK,mime,Files.readAllBytes(resourcePath));
 		}
 
+		Path indexFilePath = resourcePath.resolve("index.html");
+
+		if(Files.isDirectory(resourcePath) && Files.exists(indexFilePath)){
+			return new Response(Status.OK, HTML_MIME, Files.readAllBytes(indexFilePath));
+		}
+
+		return new Response(Status.NOT_FOUND,HTML_MIME,Files.readAllBytes(NOT_FOUND_HTML_PATH));
 	}
 
 
